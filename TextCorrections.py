87e844f7dec4c8a203506f9caa68e03414ffec62
Dimitry Rudenko <3952800@gmail.com>
2020-01-06 18:42:51 +0300

    refactoring

--- TextCorrections.py
+++ TextCorrections.py
@@ -1,5 +1,6 @@
 import re
 import unittest
+from urllib.parse import quote
 
 
 class TextCorrections:
@@ -19,6 +20,115 @@ class TextCorrections:
         if isinstance(text, str): text = text.replace(',', '.')
         return round(float(text), ndigits)
 
+    @staticmethod
+    def good_name(name):
+        name = name.replace('/', '-').replace('\\', '-').replace(' ', '-').replace('*', '-').lower()
+        name = name.replace('ä', 'ae').replace('ö', 'oe').replace('ü', 'ue').replace('ß', 'ss')
+        name = name.replace('°', '').replace('+', '-').replace('?', '-').replace('&', '-')
+        return quote(name)
+
+    @staticmethod
+    def get_cache_path(url, short=False, cache_path='C:\\Users\\Administrator\\Documents\\_python\\_cache\\'):
+        file_name = url[url.find("//") + 2:].replace('..', '').replace('/', '\\')
+        if short and '?' in file_name: file_name = file_name[:file_name.rfind("?")]
+        file_name = quote(file_name)
+        if file_name[-1] == '\\': file_name += 'html.html'
+        if '\\' not in file_name: file_name += '\\html.html'
+        right_part = file_name[file_name.rfind('\\'):]
+        if '.' not in right_part: file_name += '.html'
+        return cache_path + file_name
+
+    @staticmethod
+    def fuck_rus_letters(text):
+        text = str(text)
+        rus = 'УКЕНХВАРОСМТукехаросм'
+        eng = 'YKEHXBAPOCMTykexapocm'
+        for i, each in enumerate(rus):
+            text = re.sub(each, eng[i], text)
+        return text
+
+    @staticmethod
+    def transliterate(string):
+        capital_letters = {u'А': u'A',
+                           u'Б': u'B',
+                           u'В': u'V',
+                           u'Г': u'G',
+                           u'Д': u'D',
+                           u'Е': u'E',
+                           u'Ё': u'E',
+                           u'Ж': u'Zh',
+                           u'З': u'Z',
+                           u'И': u'I',
+                           u'Й': u'Y',
+                           u'К': u'K',
+                           u'Л': u'L',
+                           u'М': u'M',
+                           u'Н': u'N',
+                           u'О': u'O',
+                           u'П': u'P',
+                           u'Р': u'R',
+                           u'С': u'S',
+                           u'Т': u'T',
+                           u'У': u'U',
+                           u'Ф': u'F',
+                           u'Х': u'H',
+                           u'Ц': u'Ts',
+                           u'Ч': u'Ch',
+                           u'Ш': u'Sh',
+                           u'Щ': u'Sch',
+                           u'Ъ': u'',
+                           u'Ы': u'Y',
+                           u'Ь': u'',
+                           u'Э': u'E',
+                           u'Ю': u'Yu',
+                           u'Я': u'Ya', }
+        lower_case_letters = {u'а': u'a',
+                              u'б': u'b',
+                              u'в': u'v',
+                              u'г': u'g',
+                              u'д': u'd',
+                              u'е': u'e',
+                              u'ё': u'e',
+                              u'ж': u'zh',
+                              u'з': u'z',
+                              u'и': u'i',
+                              u'й': u'y',
+                              u'к': u'k',
+                              u'л': u'l',
+                              u'м': u'm',
+                              u'н': u'n',
+                              u'о': u'o',
+                              u'п': u'p',
+                              u'р': u'r',
+                              u'с': u's',
+                              u'т': u't',
+                              u'у': u'u',
+                              u'ф': u'f',
+                              u'х': u'h',
+                              u'ц': u'ts',
+                              u'ч': u'ch',
+                              u'ш': u'sh',
+                              u'щ': u'sch',
+                              u'ъ': u'',
+                              u'ы': u'y',
+                              u'ь': u'',
+                              u'э': u'e',
+                              u'ю': u'yu',
+                              u'я': u'ya', }
+        translit_string = ""
+        for index, char in enumerate(string):
+            if char in lower_case_letters.keys():
+                char = lower_case_letters[char]
+            elif char in capital_letters.keys():
+                char = capital_letters[char]
+                if len(string) > index + 1:
+                    if string[index + 1] not in lower_case_letters.keys():
+                        char = char.upper()
+                else:
+                    char = char.upper()
+            translit_string += char
+        return translit_string
+
 
 class FindDigits:
     def __init__(self, point='.,'):
@@ -31,6 +141,37 @@ class FindDigits:
         result = []
         for each in findall: result.append(float((each[2] or each[1] or each[0]).replace(',', '.')))
         return result if result else ['']
+
+
+class Html:
+    @staticmethod
+    def div_title(text, m2=False):
+        if not m2: return f'<br><br><div class="section-title">{str(text)}</div>' if text else ''
+        return f'<br><br><div class="section-title"><strong>{str(text)}</strong></div>' if text else ''
+
+    @staticmethod
+    def div_ul_start():
+        return '<ul class="bullet">'
+
+    @staticmethod
+    def div_li(text, link=None):
+        if text and link:
+            return f'<li><a href="{link}" target="_blank">{text}</a></li>'
+        elif text:
+            return f'<li>{text}</li>'
+        return ''
+
+    @staticmethod
+    def div_ul_end():
+        return '</ul>'
+
+    @staticmethod
+    def div_youtube(video_link):
+        if video_link:
+            return f'<iframe width="800" height="450" src="https://www.youtube-nocookie.com/embed/{video_link}" ' \
+                   f'frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" ' \
+                   f'allowfullscreen></iframe><br><br>'
+        return ''
 
 
 class TextCorrectionsTests(unittest.TestCase):
