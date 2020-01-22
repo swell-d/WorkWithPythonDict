753982ab8c38a75dfe2adc636b45b4f90e4a54e9
Dimitry Rudenko <3952800@gmail.com>
2020-01-22 19:39:25 +0300

    refactoring

--- Deepl.py
+++ Deepl.py
@@ -20,37 +20,37 @@ class Deepl:
         cls.__personal_dict = personal_dict.copy() if personal_dict is not None else {}
 
     @classmethod
-    def translate_text(cls, txt, source_lang, target_lang):
+    def translate(cls, txt, source_lang, target_lang):
         if cls.__DB is None: raise ValueError('use Deepl() instead Deepl')
         text, source_lang, target_lang = Sw.clr(txt), source_lang.upper(), target_lang.upper()
         if not text: return ''
-        db = cls.get_db(target_lang)
+        db = cls.__get_db(target_lang)
         if text in db: return db[text]
-        page = cls.connect_to_API(text, source_lang, target_lang)
+        page = cls.__connect_to_API(text, source_lang, target_lang)
         result = Sw.clr(json.loads(page.text)['translations'][0]['text'])
-        result = cls._personal_dict(result)
-        cls.save_stat_and_print(text, result, source_lang, target_lang)
+        result = cls.__check_personal_dict(result)
+        cls.__save_stat_and_print(text, result, source_lang, target_lang)
         db[text] = result
-        cls.save_db()
+        cls.__save_db()
         return result
 
     @classmethod
-    def _personal_dict(cls, result):
+    def __check_personal_dict(cls, result):
         for key, value in cls.__personal_dict.items():
             result.replace(key, value)
         return result
 
     @classmethod
-    def save_db(cls):
+    def __save_db(cls):
         WorkWithJSON.save_dict_to_json(cls.__DB, file_name=cls.__DB_PATH)
 
     @classmethod
-    def get_db(cls, target_lang):
+    def __get_db(cls, target_lang):
         if target_lang not in cls.__DB: cls.__DB[target_lang] = {}
         return cls.__DB[target_lang]
 
     @classmethod
-    def connect_to_API(cls, text, source_lang, target_lang):
+    def __connect_to_API(cls, text, source_lang, target_lang):
         data = {'auth_key': cls.__auth_key,
                 'text': text,
                 'target_lang': target_lang,
@@ -60,13 +60,13 @@ class Deepl:
         if page.status_code != 200:
             print(f'translate_text: page status code {page.status_code}')
             print(page.content)
-            cls.save_db()
+            cls.__save_db()
             raise ConnectionError('deepl API problem')
         page.encoding = 'utf-8'
         return page
 
     @classmethod
-    def save_stat_and_print(cls, text, result, source_lang, target_lang):
+    def __save_stat_and_print(cls, text, result, source_lang, target_lang):
         if 'summ' not in cls.__DB: cls.__DB['summ'] = 0
         cls.__DB['summ'] += len(text)
         print(f"{source_lang}: {text} ({len(text)})")
@@ -76,10 +76,10 @@ class Deepl:
 class DeeplTests(unittest.TestCase):
     def test_translate(self):
         deepl = Deepl()
-        self.assertEqual('Guten Tag!', deepl.translate_text('Hello!', source_lang='EN', target_lang='DE'))
+        self.assertEqual('Guten Tag!', deepl.translate('Hello!', source_lang='EN', target_lang='DE'))
         self.assertEqual(
             'Drücken Sie auf <i>Weiter</i>, um zur nächsten Seite zu gelangen.',
-            Deepl.translate_text(
+            Deepl.translate(
                 'Press <i>Continue</i> to advance to the next page.', source_lang='EN', target_lang='DE'
             )
         )
